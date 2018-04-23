fea90b2d9c12c0e4300e6ad9ddb30bff90c13411
beisir <hsy7426@163.com>
2018-04-24 00:06:42 +0800

    calss 滚动

--- pages/class/class.js
+++ pages/class/class.js
@@ -4,7 +4,8 @@ Page({
     data: {
         scrollTop: 0,
         asideCur: 0,
-        asideTtile: []
+        asideTtile: [],
+        domeArr: []
     },
     onLoad () {
         const _this = this;
@@ -18,39 +19,44 @@ Page({
             };
             console.log(list);
         });
-
+        let query = wx.createSelectorQuery(),
+            domeArr = [];        
+        query.selectAll('.class-container').boundingClientRect((rects) => {
+            domeArr = rects.map(item => item.top)    
+            // console.log(domeArr)
+            _this.setData({
+                domeArr: domeArr
+            });
+        }).exec();
 
     },
 
     selectAside (e) {
         const _this = this;
+        let domeArr = _this.data.domeArr;
         let { index, supcatid, id } = e.target.dataset;
-        console.log(id);
+        _this.setData({
+            asideCur: index,
+            scrollTop: domeArr[index],
+            stopScroll: true
+        });
+    },
+    scrollChange (e) {
+        // console.log(e);
+        let { domeArr, stopScroll } = this.data;
+        if (stopScroll) { return false };        
+        const _this = this;
+        let { scrollTop } = e.detail;
         let query = wx.createSelectorQuery();
-        query.select(`#${id}`).boundingClientRect()
-        query.exec(function (res) {
-            //res就是 该元素的信息 数组
-            console.log(res[0]);
-            //取高度
+        query.selectAll('.class-container').boundingClientRect((rects) => {
+            rects = rects.map(res => res.top)
+            let minarr = rects.filter(res => res < 0);
+            // var len = minarr.length;
+            let len = minarr.length === rects.length ? rects.length - 1 : minarr.length;
+            // console.log(len)
             _this.setData({
-                asideCur: index,
-                scrollTop: res[0].top
+                asideCur: len
             });
-            // _this.setData({
-            //     realWidth: res[0].width,
-            //     realHeight: res[0].height
-            // })
-            // console.log('取高度', _this.data.realHeight);
-        })
-        
-        
-        
-        
-        
-        
-        
-        // wx.pageScrollTo({
-        //     scrollTop: 0
-        // })
+        }).exec();
     }
 });
